99a1851c6bce2e16fa5e7675c9d9ddc7b1c0570d
tbistr <tbistr99@gmail.com>
2021-12-27 15:01:52 +0000

    decodeの実装

---
+++ src/test/scala/riscv.scala
@@ -0,0 +1,28 @@
+package riscv
+
+import chisel3._
+import org.scalatest.flatspec._
+import chiseltest._
+
+class FetchTest extends AnyFlatSpec with ChiselScalatestTester {
+  "mycpu" should "work through hex" in {
+    test(new Top) { c =>
+      while (!c.io.exit.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+      // peekの評価タイミングがサンプルと少し違う。
+      c.clock.step(1)
+    }
+  }
+}
+
+class DecodeTest extends AnyFlatSpec with ChiselScalatestTester {
+  "mycpu" should "work through hex" in {
+    test(new Top) { c =>
+      while (!c.io.exit.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+      c.clock.step(1)
+    }
+  }
+}
