978374343ef5fafb17951e216cc23c22c6fc5369
tbistr <tbistr99@gmail.com>
2021-12-29 08:29:45 +0000

    論理演算実装

--- src/main/scala/riscv/Core.scala
+++ src/main/scala/riscv/Core.scala
@@ -79,7 +79,13 @@ class Core extends Module {
       (inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
       (inst === SW) -> (rs1_data + imm_s_sext),
       (inst === ADD) -> (rs1_data + rs2_data),
-      (inst === SUB) -> (rs1_data + rs2_data)
+      (inst === SUB) -> (rs1_data + rs2_data),
+      (inst === AND) -> (rs1_data & rs2_data),
+      (inst === OR) -> (rs1_data & rs2_data),
+      (inst === XOR) -> (rs1_data & rs2_data),
+      (inst === ANDI) -> (rs1_data & rs2_data),
+      (inst === ORI) -> (rs1_data & rs2_data),
+      (inst === XORI) -> (rs1_data & rs2_data)
     )
   )
 
@@ -99,7 +105,18 @@ class Core extends Module {
       (inst === LW) -> io.dmem.rdata
     )
   )
-  when(inst === LW || inst === ADD || inst === ADDI || inst === SUB) {
+  when(
+    inst === LW ||
+      inst === ADD ||
+      inst === ADDI ||
+      inst === SUB ||
+      inst === AND ||
+      inst === OR ||
+      inst === XOR ||
+      inst === ANDI ||
+      inst === ORI ||
+      inst === XORI
+  ) {
     regfile(wb_addr) := wb_data
   }
 
